bfe29de5b71b5189f4a39ff36a53bf91fd434cbf
Daniel Wieczorek <daniel.pazur@gmail.com>
2024-06-13 20:41:08 +0200

    Introduce Shaft constructor exception handling unit tests

--- src/test/java/Architecture/ShaftTest.java
+++ src/test/java/Architecture/ShaftTest.java
@@ -3,6 +3,7 @@ package Architecture;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ShaftTest {
 
@@ -18,6 +19,38 @@ public class ShaftTest {
     }
 
     @Test
+    public void testConstructorIllegalDistance() {
+        Floor floor_1 = new Floor(1);
+        Floor floor_2 = new Floor(2);
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Shaft(0, floor_1, floor_2));
+        assertEquals("distance must be greater than 0", exception.getMessage());
+    }
+
+    @Test
+    public void testConstructorDuplicateNeighbour() {
+        Floor floor = new Floor(1);
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Shaft(1, floor, floor));
+        assertEquals("floorBelow and floorAbove must not be the same", exception.getMessage());
+    }
+
+    @Test
+    public void testConstructorMisalignedNeighbours() {
+        Floor floor_1 = new Floor(1);
+        Floor floor_2 = new Floor(2);
+
+        Exception exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Shaft(1, floor_2, floor_1));
+        assertEquals("floorBelow's level must be lesser than floorAbove's level", exception.getMessage());
+    }
+
+    @Test
     public void testSetDifference() {
         Floor floor_1 = new Floor(1);
         Floor floor_2 = new Floor(2);
@@ -26,4 +59,5 @@ public class ShaftTest {
         shaft.setDistance(5);
         assertEquals(5, shaft.getDistance());
     }
+
 }
