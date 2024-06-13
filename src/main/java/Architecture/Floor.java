54a8b031985a30ae88bb559b31d035ac005ce3c7
Daniel Wieczorek <daniel.pazur@gmail.com>
2024-06-13 18:34:44 +0200

    Implement Floor

--- src/main/java/Architecture/Floor.java
+++ src/main/java/Architecture/Floor.java
@@ -1,4 +1,31 @@
 package Architecture;
 
 public class Floor {
+    private final int level;
+    private Shaft shaftBelow = null;
+    private Shaft shaftAbove = null;
+
+    public Floor(int level) {
+        this.level = level;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public Shaft getShaftBelow() {
+        return shaftBelow;
+    }
+
+    public void setShaftBelow(Shaft shaftBelow) {
+        this.shaftBelow = shaftBelow;
+    }
+
+    public Shaft getShaftAbove() {
+        return shaftAbove;
+    }
+
+    public void setShaftAbove(Shaft shaftAbove) {
+        this.shaftAbove = shaftAbove;
+    }
 }
