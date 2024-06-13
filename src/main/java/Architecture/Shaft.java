246eeca88f0f15f1c237923f4f76bea11e3ec2f8
Daniel Wieczorek <daniel.pazur@gmail.com>
2024-06-13 18:40:58 +0200

    Implement Shaft

--- src/main/java/Architecture/Shaft.java
+++ src/main/java/Architecture/Shaft.java
@@ -1,4 +1,37 @@
 package Architecture;
 
 public class Shaft {
+    private int distance;
+    private Floor floorBelow = null;
+    private Floor floorAbove = null;
+
+    public Shaft(int distance, Floor floorBelow, Floor floorAbove) {
+        this.distance = distance;
+        this.floorBelow = floorBelow;
+        this.floorAbove = floorAbove;
+    }
+
+    public int getDistance() {
+        return distance;
+    }
+
+    public void setDistance(int distance) {
+        this.distance = distance;
+    }
+
+    public Floor getFloorBelow() {
+        return floorBelow;
+    }
+
+    public void setFloorBelow(Floor floorBelow) {
+        this.floorBelow = floorBelow;
+    }
+
+    public Floor getFloorAbove() {
+        return floorAbove;
+    }
+
+    public void setFloorAbove(Floor floorAbove) {
+        this.floorAbove = floorAbove;
+    }
 }
