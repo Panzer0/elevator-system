b80cf83e519f03cfd0527717ea3838ad214ae905
Daniel Wieczorek <daniel.pazur@gmail.com>
2024-06-13 21:02:05 +0200

    Introduced Elevator class stump

--- src/main/java/Elevator.java
+++ src/main/java/Elevator.java
@@ -1,2 +1,19 @@
+import Architecture.Floor;
+
+import java.util.TreeSet;
+
 public class Elevator {
+    enum Status {
+        UPWARD,
+        DOWNWARD,
+        IDLE
+    }
+
+    Status status;
+    Floor currentFloor;
+    int shaftProgress;
+    boolean waiting;
+    TreeSet<Integer> roadmap;
+    TreeSet<Integer> backlog;
+
 }
