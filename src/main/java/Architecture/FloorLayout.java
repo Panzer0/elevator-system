5245a2c18e93c9657c293848b4713a5684601624
Daniel Wieczorek <daniel.pazur@gmail.com>
2024-06-13 19:00:00 +0200

    Implement base FloorLayout functionality

--- src/main/java/Architecture/FloorLayout.java
+++ src/main/java/Architecture/FloorLayout.java
@@ -1,4 +1,34 @@
 package Architecture;
 
+import java.util.ArrayList;
+
 public class FloorLayout {
+    private final ArrayList<Floor> floors;
+
+    public FloorLayout(Floor initialFloor) {
+        this.floors = new ArrayList<>();
+        this.floors.add(initialFloor);
+    }
+
+    public Floor getTopFloor() {
+        // Requires java 21
+        return this.floors.getLast();
+    }
+
+    public void addAbove(int distance, int level) {
+        if(level <= getTopFloor().getLevel()) {
+            throw new IllegalArgumentException("New floor's level must be above the current top floor's level");
+        }
+
+        Floor newFloor = new Floor(level);
+        Shaft newShaft = new Shaft(distance, this.getTopFloor(), newFloor);
+        this.getTopFloor().setShaftAbove(newShaft);
+        newFloor.setShaftBelow(newShaft);
+
+        this.floors.add(newFloor);
+    }
+
+    public ArrayList<Floor> getFloors() {
+        return floors;
+    }
 }
