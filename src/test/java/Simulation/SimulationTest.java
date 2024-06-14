8d29951514a8ece19c995ea0bdf49a3be8ba81ff
Daniel Wieczorek <daniel.pazur@gmail.com>
2024-06-14 20:07:16 +0200

    Implement testInternalCall unit test

--- src/test/java/Simulation/SimulationTest.java
+++ src/test/java/Simulation/SimulationTest.java
@@ -1,5 +1,4 @@
 package Simulation;
-package DynamicElements;
 
 import Architecture.Floor;
 import Architecture.FloorLayout;
@@ -20,5 +19,31 @@ public class SimulationTest {
         assertEquals(3, simulation.getElevators().size());
         assertTrue(simulation.getCallBacklog().isEmpty());
     }
+
+    @Test
+    public void testInternalCall() {
+        FloorLayout layout = new FloorLayout(0);
+        layout.addAbove(1, 4);
+        Simulation simulation = new Simulation(layout, 3);
+
+        simulation.internalCall(0, 3);
+        simulation.internalCall(1, 2);
+//        #0 to 3, #1 to 2
+
+        simulation.step();
+//        #0 1 -> 3, #1 1 -> 2
+        assertEquals(1, simulation.getElevators().get(0).getPosition());
+        assertEquals(1, simulation.getElevators().get(1).getPosition());
+        assertEquals(0, simulation.getElevators().get(2).getPosition());
+
+        simulation.step();
+//        #0 2 -> 3, #1 2 -> _ (waiting)
+        simulation.step();
+//        #0 3 -> 3 (waiting), #1 2 -> _
+        assertEquals(3, simulation.getElevators().get(0).getPosition());
+        assertEquals(2, simulation.getElevators().get(1).getPosition());
+        assertEquals(0, simulation.getElevators().get(2).getPosition());
+    }
+    
 
 }
