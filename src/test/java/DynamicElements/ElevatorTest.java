6b399ac07cd6eae369031225e9fdab5c30aa5051
Daniel Wieczorek <daniel.pazur@gmail.com>
2024-06-14 17:09:08 +0200

    Add Elevator constructor unit test

---
+++ src/test/java/DynamicElements/ElevatorTest.java
@@ -0,0 +1,25 @@
+package DynamicElements;
+
+import Architecture.Floor;
+import Architecture.FloorLayout;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ElevatorTest {
+
+    @Test
+    public void testConstructor() {
+        Floor floor_1 = new Floor(1);
+        Elevator elevator = new Elevator(2, floor_1);
+
+        assertEquals(2, elevator.getId());
+        assertEquals(floor_1, elevator.getCurrentFloor());
+        assertEquals(Elevator.Status.IDLE, elevator.getStatus());
+        assertFalse(elevator.isWaiting());
+        assertTrue(elevator.getRoadmap().isEmpty());
+        assertTrue(elevator.getBacklog().isEmpty());
+    }
+
+
+}
