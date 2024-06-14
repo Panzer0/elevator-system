f659ee494eef99791ce5fbb08722141e82cacb2d
Daniel Wieczorek <daniel.pazur@gmail.com>
2024-06-14 23:08:23 +0200

    Introduce Direction docstring

--- src/main/java/Simulation/Direction.java
+++ src/main/java/Simulation/Direction.java
@@ -1,6 +1,17 @@
 package Simulation;
 
+/**
+ * The Direction enum represents the possible directions of an external call.
+ */
 public enum Direction {
+
+    /**
+     * Represents an external call requesting an elevator to go upwards.
+     */
     UP,
+
+    /**
+     * Represents an external call requesting an elevator to go downwards.
+     */
     DOWN
 }
